bc275e68deb1d14d04afd62d79b4d09947769a99
Fulvio <70729158+FulvioDiniz@users.noreply.github.com>
2023-12-06 16:09:13 -0300

    Add files via upload

---
+++ Controlador-chave-IF-UPT-main/Back_end/Banco/InserirChave.py
@@ -0,0 +1,58 @@
+# Autor: Fulvio Diniz Santos
+from .ConexãoPost import ConectarBanco
+
+table_name = "chave_table"
+
+create_table_query_chave = '''
+CREATE TABLE chave_table (
+    nome VARCHAR(255) NOT NULL,
+    situacao boolean NOT NULL
+);
+'''
+
+def valida_chave(nome_chave):
+    conn = ConectarBanco()
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM chave_table WHERE nome = %s", (nome_chave,))
+    if cur.rowcount > 0:
+        return False
+    else:
+        return True
+
+def insere_chave(nome_chave, situacao_chave):
+    conn = ConectarBanco()
+    cur = conn.cursor()
+    # Verifica se a tabela já existe
+    cur.execute(f"SELECT EXISTS (SELECT 1 FROM information_schema.tables WHERE table_name = '{table_name}')")
+    table_exists = cur.fetchone()[0]
+
+    if table_exists:
+        print("A tabela já existe.")
+        if valida_chave(nome_chave):
+            cur.execute("INSERT INTO chave_table (nome, situacao) VALUES (%s, %s)", (nome_chave, situacao_chave))
+            conn.commit()
+            cur.close()
+            conn.close()
+            print("Chave inserida com sucesso.")
+        else:
+            print("Chave já existe na tabela.")
+            cur.close()
+            conn.close()
+            return False
+    else:
+        print("A tabela não existe. Criando tabela...")
+        cur.execute(create_table_query_chave)
+        conn.commit()
+        print("Tabela criada com sucesso!")
+
+        if valida_chave(nome_chave):
+            cur.execute("INSERT INTO chave_table (nome, situacao) VALUES (%s, %s)", (nome_chave, situacao_chave))
+            conn.commit()
+            cur.close()
+            conn.close()
+            print("Chave inserida com sucesso.")
+        else:
+            print("Chave já existe na tabela.")
+            cur.close()
+            conn.close()
+            return False
